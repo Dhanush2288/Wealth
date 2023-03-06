e05cafe8ca9878a93ea2829227a15d0ee6b0f8c5
Maxwellaru <mt987@student.aru.ac.uk>
2023-03-06 18:12:21 +0000

    week

--- Wealth_management/resources/js/router/index.js
+++ Wealth_management/resources/js/router/index.js
@@ -20,8 +20,7 @@ const routes =[
     {
         path:'/home',
         component:ClientIndexhome,
-        meta: { requiresAuth: true, requiresClient: true }
-
+ 
     },
     {
         path:'/manager',
--- Wealth_management/resources/views/welcome.blade.php
+++ Wealth_management/resources/views/welcome.blade.php
@@ -7,6 +7,8 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
